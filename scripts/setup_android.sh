08b49016516db485b702fe9e91fc628e8e0b5928
NightmareFsm <nightmarefsm007@gmail.com>
2025-07-07 22:17:46 +0600

    Update setup_android.sh
    Minimal AAR - 33.8 MB

--- scripts/setup_android.sh
+++ scripts/setup_android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Download Android AAR
-ANDROID_URL="https://github.com/anonfaded/ffmpeg-kit/releases/download/v1.0/ffmpeg-kit-https-6.0-2.LTS.aar"
+ANDROID_URL="https://github.com/anonfaded/ffmpeg-kit/releases/download/v1.0/ffmpeg-kit-min-6.0-2.LTS.aar"
 mkdir -p libs
 curl -L $ANDROID_URL -o libs/com.arthenica.ffmpegkit-flutter-7.0.aar
